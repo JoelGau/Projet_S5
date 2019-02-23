d881756b37c24247e7c27d8061f90caeb8a78db3
Joel Gaudreault <joel.gaudreault@usherbrooke.ca>
2019-02-23 09:19:20 -0500

    Ajout d'un template pour uniformiser les codes matlab

---
+++ Matlab/Template.m
@@ -0,0 +1,7 @@
+%% Entête (remplacer entête par le nom de la fonction)
+% Programmeurs:             JG,
+% Date de création:         2019-02-23
+% Dernière modification:    2019-02-23
+% Description:              Description de l'utilité de la fonction.
+%                           peut être sur plusieurs
+%                           lignes.
