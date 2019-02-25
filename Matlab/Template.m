fdafd7c0fa16c56efaaad166a191799a8d811155
simonvaillancourt03 <simonvaillancourt3@gmail.com>
2019-02-25 15:40:32 -0500

    delete fichier

--- Matlab/Template.m
+++
@@ -1,8 +0,0 @@
-%% Entête
-% Programmeurs:             Initiales,
-% Date de création:         2019-02-23
-% Dernière modification:    2019-02-23
-% Description:              
-% Description de l'utilité de la fonction. Peut être sur plusieurs lignes.
-% Décrire aussi les inputs et les outputs.
-%% Core
