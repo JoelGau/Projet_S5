0b3d273a2b6841fa6fec8d514bc8ddc2f35cac91
simonvaillancourt03 <simonvaillancourt3@gmail.com>
2019-02-25 15:41:23 -0500

    Revert "delete fichier"
    This reverts commit fdafd7c0fa16c56efaaad166a191799a8d811155.

---
+++ Matlab/Template.m
@@ -0,0 +1,8 @@
+%% Entête
+% Programmeurs:             Initiales,
+% Date de création:         2019-02-23
+% Dernière modification:    2019-02-23
+% Description:              
+% Description de l'utilité de la fonction. Peut être sur plusieurs lignes.
+% Décrire aussi les inputs et les outputs.
+%% Core
