9771547810d3d4e32918c4d7f4da8231ea22f137
mlabelle22 <mathieu_message@hotmail.com>
2019-02-28 12:39:45 -0500

    Ajout des fichiers via un dossier non zip

---
+++ Projet_S5.X/LCD_SPI.X/main.c
@@ -0,0 +1,24 @@
+#include "LCD_SPI.h"
+ 
+
+ void main(void)
+ {
+     unsigned char status = 0;
+     unsigned char data[5];
+     int i;
+ 
+     // run internal oscillator at 8 MHz
+
+     
+     initialisation_LCD();
+     //OpenSPI(SPI_FOSC_16, MODE_00, SMPMID); //open SPI1
+     
+
+    
+     while (1)
+     {
+        
+     }
+ }
+ 
+ 
